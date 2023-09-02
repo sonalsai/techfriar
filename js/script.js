774bb21cee168908508301bd1394095a00eef98b
sonalsai <sonalsai777@gmail.com>
2023-09-03 00:21:47 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-/**
 function zoom() {
     document.body.style.zoom = "64%"
 }
 function zoom2() {
     document.body.style.zoom = "70%"
 }
-function zoom3() {
-    document.body.style.zoom = "65%"
-}
-*/
